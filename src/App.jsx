c07f3aa3dcf7adf70d62569c845777c67022a969
jyotiprokash <jyotiprokash2000@gmail.com>
2026-03-03 21:27:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <section className='w-300 mx-auto'>
+          <Navbar/>
+      </section>
+    </>
+  )
+}
+
+export default App
